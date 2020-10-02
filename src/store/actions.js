eef2a38b39784d04171962599cedbcf2c31d5d44
Kruxk <reinko.brink@gmail.com>
2020-10-02 14:52:53 +0200

    added function to find neighbouring mines

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,4 @@
-export const NEW_GAME = "NEW_GAME";
-
+//HELPER FUNTIONS
 const randomNum = (dimension) =>
   Math.floor(Math.random() * 1000000) % dimension;
 
@@ -23,19 +22,83 @@ const plantMines = (board, mines) => {
   const randomX = randomNum(board.length);
   const randomY = randomNum(board.length);
 
-  if (!board[randomX][randomY].isMine && mines > 0) {
+  if (mines === 0) {
+    return board;
+  } else if (board[randomX][randomY].isMine) {
+    return plantMines(board, mines);
+  } else {
     board[randomX][randomY].isMine = true;
     return plantMines(board, mines - 1);
-  } else if (mines === 0) {
-    return board;
-  } else {
-    return plantMines(board, mines);
   }
 };
 
+const getArea = (x, y, board) => {
+  const area = [];
+  const dimension = board.length;
+  //up
+  if (x > 0) {
+    area.push(board[x - 1][y]);
+  }
+  //down
+  if (x < dimension - 1) {
+    area.push(board[x + 1][y]);
+  }
+  //left
+  if (y > 0) {
+    area.push(board[x][y - 1]);
+  }
+  //right
+  if (y < dimension - 1) {
+    area.push(board[x][y + 1]);
+  }
+  //top left
+  if (x > 0 && y > 0) {
+    area.push(board[x - 1][y - 1]);
+  }
+  //top right
+  if (x > 0 && y < dimension - 1) {
+    area.push(board[x - 1][y + 1]);
+  }
+  //bottom right
+  if (x < dimension - 1 && y < dimension - 1) {
+    area.push(board[x + 1][y + 1]);
+  }
+  //bottom left
+  if (x < dimension - 1 && y > 0) {
+    area.push(board[x + 1][y - 1]);
+  }
+  return area;
+};
+
+const getNeighbours = (board) => {
+  const updatedBoard = board.reduce((acc, curr, x) => {
+    curr.map((cell, y) => {
+      if (!cell.isMine) {
+        const numOfMines = getArea(x, y, board).reduce((acc, curr) => {
+          if (curr.isMine) {
+            return acc + 1;
+          }
+          return acc;
+        }, 0);
+
+        cell.neighbours = numOfMines;
+        return cell;
+      }
+      return cell;
+    });
+    return [...acc, curr];
+  }, []);
+
+  return updatedBoard;
+};
+
+//NON HELPER FUNCTIONS
+export const NEW_GAME = "NEW_GAME";
+
 export const newGame = (dimension) => {
-  const numOfMines = Math.floor(dimension * dimension * 0.2);
+  const numOfMines = Math.floor(dimension * dimension * 0.25);
   const board = plantMines(initBoard(dimension), numOfMines);
 
+  console.log(getNeighbours(board));
   return { type: NEW_GAME, payload: board };
 };
