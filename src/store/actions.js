6b297d5c985965f6a5c10baa09e5c85592315ab9
Kruxk <reinko.brink@gmail.com>
2020-10-02 13:36:03 +0200

    added recursive function to plant mines

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,8 @@
 export const NEW_GAME = "NEW_GAME";
 
+const randomNum = (dimension) =>
+  Math.floor(Math.random() * 1000000) % dimension;
+
 const initBoard = (dimension) => {
   const board = [];
   for (let x = 0; x < dimension; x++) {
@@ -16,7 +19,23 @@ const initBoard = (dimension) => {
   return board;
 };
 
-export const newGame = (dimension) => ({
-  type: NEW_GAME,
-  payload: initBoard(dimension),
-});
+const plantMines = (board, mines) => {
+  const randomX = randomNum(board.length);
+  const randomY = randomNum(board.length);
+
+  if (!board[randomX][randomY].isMine && mines > 0) {
+    board[randomX][randomY].isMine = true;
+    return plantMines(board, mines - 1);
+  } else if (mines === 0) {
+    return board;
+  } else {
+    return plantMines(board, mines);
+  }
+};
+
+export const newGame = (dimension) => {
+  const numOfMines = Math.floor(dimension * dimension * 0.2);
+  const board = plantMines(initBoard(dimension), numOfMines);
+
+  return { type: NEW_GAME, payload: board };
+};
